d95e88484c4b433678824020b1dbe006fe08a62b
Gurshan Aulakh <shan@Gurshans-MacBook-Pro-2.local>
2024-09-05 21:31:40 -0700

    fixed the dark mode toggle button height

--- src/components/darkmode.js
+++ src/components/darkmode.js
@@ -21,7 +21,7 @@ function Darkmode (){
   return (
     <div className="flex items-center mr-0 lg:mr-8 w-16">
         <div className={`transition-all duration-1000 border-2   h-7 rounded-full w-16 flex ${ischecked?"justify-end border-white":"justify-start border-black"}`} >
-      <input type="checkbox" checked={ischecked} className={`w-6 h- rounded-full relative  appearance-none border bg-green-500`} onClick={handleclick}></input> 
+      <input type="checkbox" checked={ischecked} className={`w-6 h-6 rounded-full relative  appearance-none border bg-green-500`} onClick={handleclick}></input> 
        </div>
     </div>
   )
--- src/components/incomeandexpense.js
+++ src/components/incomeandexpense.js
@@ -4,7 +4,7 @@ import Expense from './expense.js'
  function IncomeAndExpense() {
   return (
     <div className="text-center ">
-    <div className="m-5 md:w-3/4  mx-auto flex justify-center md:justify-between rounded-3xl ">
+    <div className="m-5 md:w-full lg:w-3/4  mx-auto flex justify-center md:justify-between rounded-3xl ">
       <Income/>
       <Expense/>
     </div>
