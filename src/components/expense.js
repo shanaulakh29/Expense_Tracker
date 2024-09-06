3218a02ff541819838d767a61fcfc4c9e93adcf6
Gurshan Aulakh <shan@Gurshans-MacBook-Pro-2.local>
2024-09-05 19:45:36 -0700

    fixing the total income and total expense

--- src/components/expense.js
+++ src/components/expense.js
@@ -5,8 +5,8 @@ function Expense () {
     let newtotalexpensevalue=useSelector(getexpense)
   return (
     <div className=" rounded-3xl bg-red-500 py-4 w-1/2  md:max-w-sm flex justify-center ml-2 hover:bg-red-600 transition-all duration-1000 ">
-    <div className="text-md md:text-xl shadow-2xl lg:bg-red-600 w-3/4 rounded-full">
-    <div>Total Expenses Till Date</div>
+    <div className="text-md md:text-xl shadow-2xl p-2 lg:bg-red-600 w-3/4 rounded-lg">
+    <div>Total Expenses <span className="hidden lg:inline">Till Date</span></div>
     <div>${newtotalexpensevalue}</div>
   </div>
   </div>
--- src/components/income.js
+++ src/components/income.js
@@ -5,8 +5,8 @@ function Income(){
     let newtotalincome=useSelector(getincome);
   return (
     <div className="rounded-3xl py-4 w-1/2 md:max-w-sm flex justify-center bg-green-500 mr-2 hover:bg-green-600 transition-all duration-1000 ">
-    <div className="text-white text-md md:text-xl rounded-full shadow-2xl w-3/4 lg:bg-green-600  ">
-      <div>Total Income Till Date</div>
+    <div className="text-white  text-md md:text-xl p-2 rounded-lg shadow-2xl w-3/4 lg:bg-green-600  ">
+      <div>Total Income <span className="hidden lg:inline">Till Date</span></div>
       <div>${newtotalincome}</div>
     </div>
     </div>
